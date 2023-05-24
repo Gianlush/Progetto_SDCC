c3e1ba38b3b3960312c0e41e10b410e0b09ccf0a
Gianluca <gianluca.massara99@gmail.com>
2023-05-24 19:24:47 +0200

    aggiunti script sql DB

---
+++ src/main/resources/createAllDB/my_library_book.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `my_library` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `my_library`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: my_library
+-- ------------------------------------------------------
+-- Server version	5.6.51-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `genre` varchar(45) NOT NULL,
+  `age` varchar(50) NOT NULL,
+  `author` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (2,'50 sfumature di grigio','Romanzo','Per adulti (18+)','E. L. James'),(3,'50 sfumature di nero','Romanzo','Per adulti (18+)','E. L. James'),(4,'50 sfumature di rosso','Romanzo','Per adulti (18+)','E. L. James'),(5,'Assassinio sull\'Orient Express','Giallo ','Per ragazzi (11-17)','Agatha Christie'),(6,'La serie infernale','Giallo ','Per ragazzi (11-17)','Agatha Christie'),(7,'Fuoco e sangue','Avventura','Per ragazzi (11-17)','George R. R. Martin'),(8,'L\'ascesa del drago','Avventura','Per ragazzi (11-17)','George R. R. Martin'),(9,'Uno scontro di Re','Avventura','Per ragazzi (11-17)','George R. R. Martin'),(10,'Harry Potter e il Calice di Fuoco','Fantasy','Per ragazzi (11-17)','J. K. Rowling'),(11,'Harry Potter e il prigioniero di Azkaban','Fantasy','Per ragazzi (11-17)','J. K. Rowling'),(12,'Harry Potter e il principe Mezzosangue','Fantasy','Per ragazzi (11-17)','J. K. Rowling'),(13,'Harry Potter e i Doni della Morte','Fantasy','Per ragazzi (11-17)','J. K. Rowling'),(14,'Harry Potter e l\'Ordine della Fenice','Fantasy','Per ragazzi (11-17)','J. K. Rowling'),(15,'Harry Potter e la Camera dei Segreti','Fantasy','Per ragazzi (11-17)','J. K. Rowling'),(16,'Harry Potter e la Pietra Filosofale','Fantasy','Per ragazzi (11-17)','J. K. Rowling'),(17,'Il giro del mondo in 80 giorni','Avventura','Per bambini (3-10)','Jules Verne'),(18,'Il nome della rosa','Romanzo','Per adulti (18+)','Umberto Eco'),(19,'Il piccolo Principe','Fantasy','Per bambini (3-10)','Antoine de Saint-Exupéry'),(20,'IT','Horror','Per adulti (18+)','Stephen King'),(21,'L\'altra verità','Autobiografico','Per adulti (18+)','Alda Merini'),(22,'L\'isola misteriosa','Avventura','Per bambini (3-10)','Jules Verne'),(23,'Lungo cammino verso la libertà','Autobiografico','Per ragazzi (11-17)','Nelson Mandela'),(24,'Storia delle tenebre e dei luoghi leggendari','Romanzo','Per adulti (18+)','Umberto Eco'),(25,'The Shining','Horror','Per adulti (18+)','Stephen King'),(26,'The Outsider','Horror','Per adulti (18+)','Stephen King'),(27,'Ventimila leghe sotto i mari','Avventura','Per bambini (3-10)','Jules Verne'),(28,'Billy Budd','Romanzo','Per adulti (18+)','Herman Melville'),(29,'Io, Nelson Mandela','Autobiografico','Per adulti (18+)','Nelson Mandela'),(30,'Io sono Malala','Autobiografico','Per ragazzi (11-17)','Christina Lamb'),(31,'La compagnia dell\'anello','Avventura','Per ragazzi (11-17)','J. R. R. Tolkien'),(32,'Lettere dal carcere','Autobiografico','Per adulti (18+)','Antonio Gramsci'),(33,'Le due torri','Avventura','Per ragazzi (11-17)','J. R. R. Tolkien'),(34,'Lo Hobbit','Avventura','Per ragazzi (11-17)','J. R. R. Tolkien'),(35,'Moby Dick','Avventura','Per ragazzi (11-17)','Herman Melville'),(36,'Taipi','Romanzo','Per ragazzi (11-17)','Herman Melville'),(37,'Terra degli uomini','Autobiografico','Per ragazzi (11-17)','Antoine de Saint-Exupéry'),(38,'Una persona alla volta','Autobiografico','Per ragazzi (11-17)','Gino Strada'),(39,'Una terra promessa','Autobiografico','Per adulti (18+)','Barack Obama'),(40,'Volo di notte','Romanzo','Per ragazzi (11-17)','Antoine de Saint-Exupéry');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-24 19:24:08
---
+++ src/main/resources/createDataDB/my_library_user.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE  IF NOT EXISTS `my_library` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `my_library`;
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: my_library
+-- ------------------------------------------------------
+-- Server version	5.6.51-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (4,'Gianluca','Massara','gianluca.massara99@gmail.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(75,'Murphy','Diane','dmurphy@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(76,'Patterson','Mary','mpatterso@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(77,'Firrelli','Jeff','jfirrelli@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(78,'Patterson','William','wpatterson@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(79,'Bondur','Gerard','gbondur@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(80,'Bow','Anthony','abow@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(81,'Jennings','Leslie','ljennings@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(82,'Thompson','Leslie','lthompson@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(83,'Firrelli','Julie','jfirrelli@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(84,'Patterson','Steve','spatterson@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(85,'Tseng','Foon Yue','ftseng@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(86,'Vanauf','George','gvanauf@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(87,'Bondur','Loui','lbondur@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(88,'Hernandez','Gerard','ghernande@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(89,'Castillo','Pamela','pcastillo@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(90,'Bott','Larry','lbott@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(91,'Jones','Barry','bjones@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(92,'Fixter','Andy','afixter@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(93,'Marsh','Peter','pmarsh@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(94,'King','Tom','tking@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(95,'Nishi','Mami','mnishi@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(96,'Kato','Yoshimi','ykato@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(97,'Kato','Pamela','mgerd@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(98,'Tseng','Bondur','feng@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(99,'Bondur','Loui','lndur@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(100,'Loui','Pamela','pcasllo@classicmodelcars.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8'),(110,'Gianluca','Massara','gianluca@mail.com','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-24 19:23:54
