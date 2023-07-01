a6738a8e086efbdf8162d30ed8635db6a35fea6d
Gianluca Massara <62150736+Gianlush@users.noreply.github.com>
2023-07-01 22:22:00 +0200

    merge

---
+++ Frontend_SDCC_Flutter/lib/pages/HomePage.dart
@@ -0,0 +1,383 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_sdcc_flutter/object/Book.dart';
+import 'package:frontend_sdcc_flutter/pages/LoginPage.dart';
+import 'package:frontend_sdcc_flutter/widget/BookCover.dart';
+
+import '../utility/Model.dart';
+import '../widget/Logo.dart';
+
+class HomePage extends StatefulWidget {
+
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController textController;
+  Map<String, bool> autori;
+  List<String> autoriSelezionati;
+  List<String> generiSelezionati;
+  List<String> etaSelezionate;
+  Map<String, bool> eta;
+  Map<String, bool> genere;
+  List<Book> books = [];
+
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController(text: "");
+    autoriSelezionati = [];
+    etaSelezionate = [];
+    generiSelezionati = [];
+
+    autori = {
+      "Antoine de Saint-Exupéry" : false,
+      "Agatha Christie" : false,
+      "Alda Merini" : false,
+      "Antonio Gramsci" : false,
+      "Barack Obama" : false,
+      "Christina Lamb" : false,
+      "E. L. James" : false,
+      "George R. R. Martin" : false,
+      "Gino Strada" : false,
+      "Herman Melville" : false,
+      "J. K. Rowling" : false,
+      "J. R. R. Tolkien" : false,
+      "Jules Verne" : false,
+      "Nelson Mandela" : false,
+      "Stephen King" : false,
+      "Umberto Eco" : false,
+    };
+
+    eta = {
+      "Per bambini (3-10)" : false,
+      "Per ragazzi (11-17)" : false,
+      "Per adulti (18+)" : false
+    };
+
+    genere = {
+      "Autobiografico" : false,
+      "Avventura" : false,
+      "Fantasy" : false,
+      "Giallo" : false,
+      "Horror" : false,
+      "Romanzo" : false,
+    };
+    search();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                color: Colors.black,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(12, 0, 15, 5),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Padding(
+                              padding: EdgeInsets.fromLTRB(10, 10, 0, 35),
+                              child: Logo(color: Colors.white)
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
+                            child: RawMaterialButton(
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20), side: const BorderSide(color: Colors.blue, width: 2)),
+                              padding: const EdgeInsets.fromLTRB(10,10,10,10),
+                              onPressed: () => logout(),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [
+                                  Text("Logout",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25
+                                    ),
+                                  ),
+                                  Padding(
+                                      padding: EdgeInsets.fromLTRB(8, 2, 0, 0),
+                                      child: Icon(Icons.account_circle_outlined, size: 25, color: Colors.blueAccent,)
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width*0.15, 0, 0, 0),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width*0.6,
+                          decoration: BoxDecoration(
+                            border: const Border.fromBorderSide(
+                                BorderSide(
+                                    color: Colors.black54,
+                                    width: 2
+                                )
+                            ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: TextFormField(
+                                    controller: textController,
+                                    style: const TextStyle(
+                                        color: Colors.black
+                                    ),
+                                    obscureText: false,
+                                    decoration: InputDecoration(
+                                      prefixIcon: const Icon(Icons.search, color: Colors.black54),
+                                      labelStyle: const TextStyle(
+                                          color: Colors.white
+                                      ),
+                                      hintText: 'Cerca qui il tuo Libro...',
+                                      hintStyle: const TextStyle(
+                                          color: Colors.black54
+                                      ),
+                                      enabledBorder: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                          color: Colors.white,
+                                          width: 2,
+                                        ),
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderSide: const BorderSide(
+                                          color: Colors.white,
+                                          width: 2,
+                                        ),
+                                        borderRadius: BorderRadius.circular(30),
+                                      ),
+                                      filled: true,
+                                      fillColor: Colors.white,
+                                      contentPadding: const EdgeInsetsDirectional.fromSTEB(16, 24, 0, 24),
+                                    ),
+                                  )
+                              ),
+                              Padding(
+                                  padding: const EdgeInsets.fromLTRB(0, 0, 5, 0),
+                                  child: RawMaterialButton(
+                                      onPressed: () => search(),
+                                      padding: const EdgeInsets.fromLTRB(30,10,30,10),
+                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                                      fillColor: Colors.indigo,
+                                      child: const Text("Cerca",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontFamily: 'Urbanist',
+                                            fontSize: 18
+                                        ),
+                                      )
+                                  )
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(padding: EdgeInsets.fromLTRB(15, 50, 0, 0),
+                        child: Text("Genere",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                          width: 175,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                            child: Column(
+                              children:
+                              genere.keys.map((String key) {
+                                return CheckboxListTile(
+                                  dense: true,
+                                  visualDensity: const VisualDensity(vertical: VisualDensity.minimumDensity),
+                                  title: Text(key,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 13,
+                                      )
+                                  ),
+                                  side: const BorderSide(color: Colors.black),
+                                  activeColor: Colors.blueAccent,
+                                  value: genere[key],
+                                  onChanged: (bool value) => genreCheck(value,key),
+                                );
+                              }).toList(),
+                            ),
+                          )
+                      ),
+                      const Padding(padding: EdgeInsets.fromLTRB(15, 25, 0, 0),
+                        child: Text("Autore",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                          width: 175,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                            child: Column(
+                              children:
+                              autori.keys.map((String key) {
+                                return CheckboxListTile(
+                                  dense: true,
+                                  visualDensity: const VisualDensity(vertical: VisualDensity.minimumDensity),
+                                  title: Text(key,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 13,
+                                      )
+                                  ),
+                                  side: const BorderSide(color: Colors.black),
+                                  activeColor: Colors.blueAccent,
+                                  value: autori[key],
+                                  onChanged: (bool value) => authorsCheck(value,key),
+                                );
+                              }).toList(),
+                            ),
+                          )
+                      ),
+                      const Padding(padding: EdgeInsets.fromLTRB(15, 25, 0, 0),
+                        child: Text("Età",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                          width: 175,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                            child: Column(
+                              children:
+                              eta.keys.map((String key) {
+                                return CheckboxListTile(
+                                  dense: true,
+                                  visualDensity: const VisualDensity(vertical: VisualDensity.minimumDensity),
+                                  title: Text(key,
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 13,
+                                      )
+                                  ),
+                                  side: const BorderSide(color: Colors.black),
+                                  activeColor: Colors.blueAccent,
+                                  value: eta[key],
+                                  onChanged: (bool value) => ageCheck(value,key),
+                                );
+                              }).toList(),
+                            ),
+                          )
+                      )
+                    ],
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(50, 25, 20, 0),
+                      child: Wrap(
+                        spacing: 50,
+                        runSpacing: 30,
+                        children: books.map((Book key) {
+                          return BookCover(book: key, proportion: 1, clickable: true);
+                        }).toList(),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+      ),
+    );
+  }
+
+  search(){
+    Model.sharedInstance.searchBook(name:textController.text, ages: etaSelezionate, authors: autoriSelezionati, genres: generiSelezionati).then((value) {
+      setState(() {
+        books = value;
+      });
+    });
+  }
+
+  logout(){
+    setState(() {
+      LoginPage.userLogged = null;
+    });
+    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => const LoginPage()));
+  }
+
+  authorsCheck(bool value, String key) {
+    setState(() {
+      autori[key] = value;
+      if(value) {
+        autoriSelezionati.add(key);
+      } else {
+        autoriSelezionati.remove(key);
+      }
+    });
+    search();
+  }
+
+
+  ageCheck(bool value, String key) {
+    setState(() {
+      eta[key] = value;
+      if(value) {
+        etaSelezionate.add(key);
+      } else {
+        etaSelezionate.remove(key);
+      }
+    });
+    search();
+  }
+
+
+  genreCheck(bool value, String key) {
+    setState(() {
+      genere[key] = value;
+      if(value) {
+        generiSelezionati.add(key);
+      } else {
+        generiSelezionati.remove(key);
+      }
+    });
+    search();
+  }
+
+}
---
+++ Frontend_SDCC_Flutter/lib/utility/Constants.dart
@@ -0,0 +1,42 @@
+class Constants{
+  static const String REALM = "myRealm";
+  static const String TITLE_APP = "MyLibrary";
+  static const String CLIENT_ID = "MyLibrary";
+
+  static const String CLIENT_ID_MASTER = "admin-cli";
+  static const String USERNAME_MASTER = "gianluca";
+  static const String PASSWORD_MASTER = "massara";
+
+  static const String REQUEST_LOGIN_MASTER="/auth/realms/master/protocol/openid-connect/token";
+  static const String REQUEST_LOGOUT_MASTER="/auth/realms/master/protocol/openid-connect/logout";
+
+
+  static const String ADDRESS_AUTHENTICATION_SERVER = "localhost:8080";
+  //versione progetto in locale
+  static const String ADDRESS_STORE_SERVER = "localhost:8081";
+  //versione progetto in Cloud
+  //static const String ADDRESS_STORE_SERVER = "51.120.0.147:8081";
+
+  static const String REQUEST_LOGIN = "/user/login";
+  static const String REQUEST_SIGNUP = "/user/register";
+
+  static const String REQUEST_SEARCH_BOOK = "/books";
+  static const String REQUEST_SEARCH_BOOK_BY_AUTHORS = "/books/authors";
+  static const String REQUEST_SEARCH_BOOK_BY_GENRES = "/books/genres";
+  static const String REQUEST_SEARCH_BOOK_BY_AGE = "/books/age";
+  static const String REQUEST_SEARCH_BY_GENRE = "/games/genre";
+  static const String REQUEST_ADD_USER = "/users";
+  static const String REQUEST_USER_BY_EMAIL = "/users/email";
+  static const String REQUEST_GET_OR_CREATE_ORDER = "/orders";
+  static const String REQUEST_SEARCH_REVIEW_OWN = "/reviews/own";
+  static const String REQUEST_SEARCH_REVIEW_ALL = "/reviews/all";
+  static const String REQUEST_SEARCH_REVIEW_KEYWORD = "/reviews/keyword";
+  static const String REQUEST_SEARCH_REVIEW_STAR = "/reviews/star";
+  static const String REQUEST_SEARCH_REVIEW_STAR_KEYWORD = "/reviews/star_keyword";
+  static const String REQUEST_SEARCH_REVIEW_SAVE = "/reviews/save";
+
+
+
+  static const String RESPONSE_ERROR_MAIL_ALREADY_EXIST ="ERROR_MAIL_ALREADY_EXIST";
+
+}
